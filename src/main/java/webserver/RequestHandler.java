de6c56a14942a67e3bf06d3545a3cfbb090767dd
kseysh <kseysh123@naver.com>
2025-12-30 12:07:33 +0900

    feat: RequestHandler 구현

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -1,18 +1,23 @@
 package webserver;
 
+import enums.HttpHeader;
 import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.Socket;
 
+import java.util.Map;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import servlet.DispatcherServlet;
+import webserver.http.HttpRequest;
+import webserver.http.HttpResponse;
 
 public class RequestHandler implements Runnable {
     private static final Logger logger = LoggerFactory.getLogger(RequestHandler.class);
 
-    private Socket connection;
+    private final Socket connection;
 
     public RequestHandler(Socket connectionSocket) {
         this.connection = connectionSocket;
@@ -22,34 +27,27 @@ public class RequestHandler implements Runnable {
         logger.debug("New Client Connect! Connected IP : {}, Port : {}", connection.getInetAddress(),
                 connection.getPort());
 
-        try (InputStream in = connection.getInputStream(); OutputStream out = connection.getOutputStream()) {
-            // TODO 사용자 요청에 대한 처리는 이 곳에 구현하면 된다.
-            DataOutputStream dos = new DataOutputStream(out);
-            byte[] body = "<h1>Hello World</h1>".getBytes();
-            response200Header(dos, body.length);
-            responseBody(dos, body);
-        } catch (IOException e) {
+        try (InputStream in = connection.getInputStream(); OutputStream out = connection.getOutputStream()){
+            HttpRequest request = new HttpRequest(in);
+            DispatcherServlet dispatcherServlet = DispatcherServlet.getInstance();
+            HttpResponse response = dispatcherServlet.doService(request);
+            sendMessage(response, out);
+        }catch (IOException e) {
             logger.error(e.getMessage());
         }
     }
 
-    private void response200Header(DataOutputStream dos, int lengthOfBodyContent) {
-        try {
-            dos.writeBytes("HTTP/1.1 200 OK \r\n");
-            dos.writeBytes("Content-Type: text/html;charset=utf-8\r\n");
-            dos.writeBytes("Content-Length: " + lengthOfBodyContent + "\r\n");
-            dos.writeBytes("\r\n");
-        } catch (IOException e) {
-            logger.error(e.getMessage());
-        }
-    }
+    private void sendMessage(HttpResponse response, OutputStream out) throws IOException {
+        DataOutputStream dos = new DataOutputStream(out);
+        dos.writeBytes(response.getHttpVersionAndResponseCode());
 
-    private void responseBody(DataOutputStream dos, byte[] body) {
-        try {
-            dos.write(body, 0, body.length);
-            dos.flush();
-        } catch (IOException e) {
-            logger.error(e.getMessage());
+        Map<String, String> headers = response.getHeaders();
+        for (Map.Entry<String, String> header : headers.entrySet()) {
+            dos.writeBytes(header.getKey() + ": " + header.getValue() + "\r\n");
         }
+        dos.writeBytes("\r\n");
+
+        dos.write(response.getBody(), 0, response.getBody().length);
+        dos.flush();
     }
 }
