bb3491e60c8596104bea0772fcf4507ecd53aec6
kseysh <kseysh123@naver.com>
2025-12-30 16:03:25 +0900

    feat: HttpStatus 추가

--- src/main/java/enums/HttpStatus.java
+++ src/main/java/enums/HttpStatus.java
@@ -3,6 +3,7 @@ package enums;
 public enum HttpStatus {
     OK("200", "OK"),
     NOT_FOUND("404", "Not Found"),
+    BAD_REQUEST("400", "Bad Request"),
     INTERNAL_SERVER_ERROR("500", "Internal Server Error"),;
 
     private final String responseCode;
