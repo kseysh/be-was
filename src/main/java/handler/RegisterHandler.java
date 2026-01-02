2a749a38543e947acfa6795ba1bc1bf879e94772
kseysh <kseysh123@naver.com>
2026-01-02 11:58:02 +0900

    refactor: GET 메서드만 허용하도록 수정

--- src/main/java/handler/RegisterHandler.java
+++ src/main/java/handler/RegisterHandler.java
@@ -1,12 +1,22 @@
 package handler;
 
+import enums.HttpMethod;
 import exception.HttpException;
+import exception.NotFoundException;
 import http.HttpRequest;
 import http.HttpResponse;
 
 public class RegisterHandler implements Handler{
     @Override
     public void handle(HttpRequest request, HttpResponse response) throws HttpException {
+        if (request.getMethod() == HttpMethod.GET) {
+            get(request, response);
+        }else{
+            throw new NotFoundException("Not Supported Method");
+        }
+    }
+
+    private void get(HttpRequest request, HttpResponse response) throws HttpException {
         request.setPath("/registration/index.html");
         new StaticResourceHandler().handle(request, response);
     }
