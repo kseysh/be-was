91e9f4b8ebaed56d972eb891a4b21ecf415e0e1f
kseysh <kseysh123@naver.com>
2026-01-02 12:07:12 +0900

    refactor: 파라미터를 제공하지 않았을 시 에러메시지 구체화

--- src/main/java/handler/CreateUserHandler.java
+++ src/main/java/handler/CreateUserHandler.java
@@ -4,6 +4,8 @@ import db.Database;
 import enums.HttpHeader;
 import enums.HttpMethod;
 import enums.HttpStatus;
+import exception.BadRequestException;
+import exception.HttpException;
 import exception.NotFoundException;
 import http.HttpRequest;
 import http.HttpResponse;
@@ -17,7 +19,7 @@ public class CreateUserHandler implements Handler {
     private static final Logger logger = LoggerFactory.getLogger(CreateUserHandler.class);
 
     @Override
-    public void handle(HttpRequest request, HttpResponse response) {
+    public void handle(HttpRequest request, HttpResponse response) throws HttpException {
         if(request.getMethod() == HttpMethod.GET) {
             get(request, response);
         }else{
@@ -25,16 +27,13 @@ public class CreateUserHandler implements Handler {
         }
     }
 
-    public void get(HttpRequest request, HttpResponse response){
+    public void get(HttpRequest request, HttpResponse response) throws HttpException {
         String userId = request.getQuery().get("userId");
         String password = request.getQuery().get("password");
         String name = request.getQuery().get("name");
         String email = request.getQuery().get("email");
 
-        if (userId == null || password == null || name == null || email == null) {
-            response.setVersion(request.getVersion());
-            response.setStatusCode(HttpStatus.BAD_REQUEST);
-        }
+        validateParameters(userId, password, name, email);
 
         User user = new User(userId, password, name, email);
         Database.addUser(user);
@@ -46,5 +45,50 @@ public class CreateUserHandler implements Handler {
         headers.put(HttpHeader.LOCATION.getValue(), "/index.html");
         headers.put(HttpHeader.CONTENT_TYPE.getValue(), "text/html");
         response.setHeaders(headers);
+    }
+
+    private void validateParameters(String userId, String password, String name, String email) throws HttpException {
+        int requiredParameterCount = 0;
+        StringBuilder sb = new StringBuilder();
+
+        if(userId == null){
+            sb.append("userId");
+            requiredParameterCount++;
+        }
+
+        if(password == null){
+            if(requiredParameterCount == 0){
+                sb.append("password");
+            }else{
+                sb.append(", password");
+            }
+            requiredParameterCount++;
+        }
+
+        if(name == null){
+            if(requiredParameterCount == 0){
+                sb.append("name");
+            }else{
+                sb.append(", name");
+            }
+            requiredParameterCount++;
+        }
+
+        if(email == null){
+            if(requiredParameterCount == 0){
+                sb.append("email");
+            }else{
+                sb.append(", email");
+            }
+        }
+
+        if(requiredParameterCount != 0){
+            if(requiredParameterCount == 1){
+                sb.append("is required");
+            }else{
+                sb.append("are required");
+            }
+            throw new BadRequestException(sb.toString());
+        }
     }
 }
