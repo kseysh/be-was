3c48c828205221056ecbeb38af2317669872ad20
kseysh <kseysh123@naver.com>
2026-01-02 11:14:53 +0900

    refactor: newFixedThreadPool을 세세하게 설정을 변경할 수 있도록 변경

--- src/main/java/webserver/WebServer.java
+++ src/main/java/webserver/WebServer.java
@@ -4,7 +4,9 @@ import java.net.ServerSocket;
 import java.net.Socket;
 
 import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -12,9 +14,16 @@ public class WebServer {
     private static final Logger logger = LoggerFactory.getLogger(WebServer.class);
     private static final int DEFAULT_PORT = 8080;
     private static final int THREAD_POOL_SIZE = 100;
-    private static final ExecutorService executor = Executors.newFixedThreadPool(THREAD_POOL_SIZE);
+    private static final ExecutorService executor = new ThreadPoolExecutor(
+            THREAD_POOL_SIZE, // Thread Pool min Size
+            THREAD_POOL_SIZE, // Thread Pool max Size
+            0, // Keep Alive Time
+            TimeUnit.MILLISECONDS, // Keep Alive Time 단위
+            new LinkedBlockingQueue<>(1000), // 대기열 크기 제한
+            new ThreadPoolExecutor.AbortPolicy() // BlockingQueue가 꽉 차면 예외 발생
+    );
 
-    public static void main(String args[]) throws Exception {
+    public static void main(String[] args) throws Exception {
         int port;
         if (args == null || args.length == 0) {
             port = DEFAULT_PORT;
