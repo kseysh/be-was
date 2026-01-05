b804efb6d77b2be7ece6553fdf257401a62c520c
kseysh <kseysh123@naver.com>
2026-01-05 18:50:03 +0900

    feat: Database의 동시성 보장 테스트 추가

---
+++ src/test/java/db/DatabaseTest.java
@@ -0,0 +1,40 @@
+package db;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.UUID;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import model.User;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class DatabaseTest {
+
+    @Test
+    @DisplayName("동시 요청이 들어와도 정합성을 유지한다.")
+    void concurrencyTest() throws InterruptedException {
+        // given
+        int threadCount = 100;
+        ExecutorService executorService = Executors.newFixedThreadPool(10);
+        CountDownLatch latch = new CountDownLatch(threadCount);
+
+        // when
+        for (int i = 0; i < threadCount; i++) {
+            executorService.submit(() -> {
+                try {
+                    String randomId = UUID.randomUUID().toString();
+                    Database.addUser(new User(randomId, "testPassword", "testName", "testEmail"));
+                } finally {
+                    latch.countDown();
+                }
+            });
+        }
+        latch.await();
+
+        // then
+        assertEquals(threadCount, Database.findAll().size());
+    }
+
+}
