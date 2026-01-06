cbef41d10e945fb9d5737df63f59b339b6a43c76
kseysh <kseysh123@naver.com>
2026-01-06 13:52:37 +0900

    feat: fixture를 사용해 HttpRequest를 생성하도록 수정

---
+++ src/test/java/fixture/HttpRequestFixture.java
@@ -0,0 +1,44 @@
+package fixture;
+
+import enums.ContentTypes;
+import enums.HttpHeader;
+import enums.HttpMethod;
+import http.HttpHeaders;
+import http.HttpRequest;
+import http.HttpRequestBody;
+import http.HttpRequestLine;
+import java.util.Map;
+
+public class HttpRequestFixture {
+
+    // requestLine
+    public static final HttpMethod DEFAULT_HTTP_METHOD = HttpMethod.GET;
+    public static final String DEFAULT_PATH = "/test";
+    public static final String DEFAULT_VERSION = "HTTP/1.1";
+    public static final String DEFAULT_QUERY_KEY = "testQueryKey";
+    public static final String DEFAULT_QUERY_VALUE = "testQueryValue";
+    public static final Map<String, String> DEFAULT_QUERIES = Map.of(DEFAULT_QUERY_KEY, DEFAULT_QUERY_VALUE);
+    public static final HttpRequestLine DEFAULT_REQUEST_LINE = new HttpRequestLine(
+            DEFAULT_HTTP_METHOD, DEFAULT_PATH, DEFAULT_VERSION, DEFAULT_QUERIES
+    );
+
+    // request body
+    public static final byte[] DEFAULT_BODY = "test body".getBytes();
+    public static final HttpRequestBody DEFAULT_HTTP_REQUEST_BODY = new HttpRequestBody(DEFAULT_BODY);
+
+    // request header
+    public static final String DEFAULT_HEADER_KEY = HttpHeader.CONTENT_TYPE.getValue();
+    public static final String DEFAULT_HEADER_VALUE = ContentTypes.APPLICATION_JSON.getMimeType();
+    public static final Map<String, String[]> DEFAULT_HEADERS = Map.of(
+            DEFAULT_HEADER_KEY, new String[]{DEFAULT_HEADER_VALUE},
+            HttpHeader.CONTENT_LENGTH.getValue(), new String[]{Integer.toString(DEFAULT_BODY.length)}
+    );
+    public static final HttpHeaders DEFAULT_HTTP_HEADERS = new HttpHeaders(DEFAULT_HEADERS);
+
+    // http request
+    public static final HttpRequest DEFAULT_HTTP_REQUEST = new HttpRequest(
+            DEFAULT_REQUEST_LINE,
+            DEFAULT_HTTP_HEADERS,
+            DEFAULT_HTTP_REQUEST_BODY
+    );
+}
--- src/test/java/http/RequestParserTest.java
+++ src/test/java/http/RequestParserTest.java
@@ -1,14 +1,11 @@
 package http;
 
+import static fixture.HttpRequestFixture.*;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.assertj.core.api.Assertions.assertThat;
-import enums.ContentTypes;
-import enums.HttpHeader;
-import enums.HttpMethod;
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.util.HashMap;
 import java.util.Map;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -19,28 +16,9 @@ class RequestParserTest {
     @DisplayName("Request를 Parsing할 수 있다.")
     void parseRequestTest() throws IOException {
         // given
-        final HttpMethod method = HttpMethod.GET;
-        final String path = "/test";
-        final String version = "HTTP/1.1";
-        final String queryKey = "testQueryKey";
-        final String queryValue = "testQueryValue";
-        final Map<String, String> queries = new HashMap<>();
-        queries.put(queryKey, queryValue);
-        HttpRequestLine requestLine = new HttpRequestLine(method, path, version, queries);
-
-        final Map<String, String[]> headers = new HashMap<>();
-        final String headerKey = HttpHeader.CONTENT_TYPE.getValue();
-        final String headerValue = ContentTypes.APPLICATION_JSON.getMimeType();
-        headers.put(headerKey, new String[]{headerValue});
-        HttpHeaders httpHeaders = new HttpHeaders(headers);
-
-        final byte[] body = "hello body".getBytes();
-        HttpRequestBody requestBody = new HttpRequestBody(body);
-        headers.put(HttpHeader.CONTENT_LENGTH.getValue(), new String[]{Integer.toString(body.length)});
+        InputStream in = new ByteArrayInputStream(DEFAULT_HTTP_REQUEST.writeHttpRequest().getBytes());
 
         // when
-        HttpRequest httpRequest = new HttpRequest(requestLine, httpHeaders, requestBody);
-        InputStream in = new ByteArrayInputStream(httpRequest.writeHttpRequest().getBytes());
         RequestParser parser = new RequestParser(in);
 
         HttpRequestLine parsedRequestLine = parser.getRequestLine();
@@ -49,16 +27,16 @@ class RequestParserTest {
 
         // then
         // 1. Request Line 검증
-        assertEquals(method, parsedRequestLine.getMethod());
-        assertEquals(path, parsedRequestLine.getPath());
-        assertEquals(version, parsedRequestLine.getVersion());
-        assertThat(parsedRequestLine.getQueries()).containsExactlyEntriesOf(queries);
+        assertEquals(DEFAULT_HTTP_METHOD, parsedRequestLine.getMethod());
+        assertEquals(DEFAULT_PATH, parsedRequestLine.getPath());
+        assertEquals(DEFAULT_VERSION, parsedRequestLine.getVersion());
+        assertThat(parsedRequestLine.getQueries()).containsExactlyEntriesOf(DEFAULT_QUERIES);
 
         // 2. Header 검증 (String[] 내용 비교)
-        assertThat(parsedHeaders).containsExactlyEntriesOf(headers);
+        assertThat(parsedHeaders).containsExactlyInAnyOrderEntriesOf(DEFAULT_HEADERS);
 
         // 3. Body 검증 (byte[] 내용 비교)
-        assertEquals(body.length, parsedBody.length);
-        assertEquals(new String(body), new String(parsedBody));
+        assertEquals(DEFAULT_HTTP_REQUEST_BODY.getBody().length, parsedBody.length);
+        assertEquals(new String(DEFAULT_HTTP_REQUEST_BODY.getBody()), new String(parsedBody));
     }
 }
