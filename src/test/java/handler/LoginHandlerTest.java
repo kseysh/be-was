7bdcf25a2e30a3bc026b6b4e4af7c58cc84ddceb
kseysh <kseysh123@naver.com>
2026-01-08 18:44:33 +0900

    feat: 로그인 핸들러 기능 및 테스트 추가

---
+++ src/test/java/handler/LoginHandlerTest.java
@@ -0,0 +1,146 @@
+package handler;
+
+import static fixture.HttpRequestFixture.DEFAULT_HTTP_HEADERS;
+import static fixture.HttpRequestFixture.DEFAULT_HTTP_REQUEST_BODY;
+import static fixture.HttpRequestFixture.DEFAULT_QUERIES;
+import static fixture.HttpRequestFixture.DEFAULT_VERSION;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import db.Database;
+import enums.ContentTypes;
+import enums.HttpHeader;
+import enums.HttpMethod;
+import enums.HttpStatus;
+import exception.NotFoundException;
+import http.request.HttpHeaders;
+import http.request.HttpRequest;
+import http.request.HttpRequestBody;
+import http.request.HttpRequestLine;
+import http.response.HttpResponse;
+import java.nio.charset.StandardCharsets;
+import java.util.HashMap;
+import java.util.Map;
+import model.User;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import util.FileReader;
+
+class LoginHandlerTest {
+
+    @Test
+    @DisplayName("/login에 GET요청을 보내면 /login/index.html로 변환하여 반환한다.")
+    void loginHandlerTest(){
+        // given
+        String requestPath = "/login";
+        String expectedPath = "/login/index.html";
+        HttpRequestLine requestLine = new HttpRequestLine(
+                HttpMethod.GET,
+                requestPath,
+                DEFAULT_VERSION,
+                DEFAULT_QUERIES
+        );
+        HttpRequest request = new HttpRequest(requestLine, DEFAULT_HTTP_HEADERS, DEFAULT_HTTP_REQUEST_BODY);
+
+        // when
+        HttpResponse response = new HttpResponse();
+        LoginHandler.getInstance().handle(request, response);
+
+        // then
+
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+        assertThat(response.getBody()).containsExactly(FileReader.readFile(expectedPath));
+        assertEquals(
+                response.getHeaders().get(HttpHeader.CONTENT_TYPE.getValue()),
+                ContentTypes.TEXT_HTML.getMimeType()
+        );
+    }
+
+    @Test
+    @DisplayName("/login에 POST요청을 보낼 때,유저가 존재하지 않으면 Not Found 에러를 반환한다.")
+    void throwNotFoundExceptionWhenUserNotFound(){
+        // given
+        String requestPath = "/login";
+        HttpRequestLine requestLine = new HttpRequestLine(
+                HttpMethod.POST,
+                requestPath,
+                DEFAULT_VERSION,
+                DEFAULT_QUERIES
+        );
+
+        String userId = "testUserId";
+        String password = "testPassword";
+        String urlEncodedFormat = "userId=%s&password=%s";
+        String urlEncodedString = urlEncodedFormat.formatted(userId, password);
+
+        Map<String, String[]> headers = new HashMap<>();
+        headers.put(
+                HttpHeader.CONTENT_TYPE.getValue(),
+                new String[]{ContentTypes.APPLICATION_FORM_URL_ENCODED.getMimeType()}
+        );
+        HttpHeaders httpHeaders = new HttpHeaders(headers);
+
+        HttpRequest request = new HttpRequest(
+                requestLine,
+                httpHeaders,
+                new HttpRequestBody(urlEncodedString.getBytes(StandardCharsets.UTF_8))
+        );
+
+        // when & then
+        assertThrows(NotFoundException.class, () -> LoginHandler.getInstance().handle(request, new HttpResponse()));
+    }
+
+    @Test
+    @DisplayName("/login에 POST요청을 보내면 Set-Cookie header를 반환한다")
+    void shouldReturnSetCookieHeaderWhenPostRequest(){
+        // given
+        String requestPath = "/login";
+        String expectedPath = "/index.html";
+        HttpRequestLine requestLine = new HttpRequestLine(
+                HttpMethod.POST,
+                requestPath,
+                DEFAULT_VERSION,
+                DEFAULT_QUERIES
+        );
+
+        String userId = "testUserId";
+        String password = "testPassword";
+        User user = new User(userId, password, "name", "email");
+        Database.addUser(user);
+
+        String urlEncodedFormat = "userId=%s&password=%s";
+        String urlEncodedString = urlEncodedFormat.formatted(userId, password);
+
+        Map<String, String[]> headers = new HashMap<>();
+        headers.put(
+                HttpHeader.CONTENT_TYPE.getValue(),
+                new String[]{ContentTypes.APPLICATION_FORM_URL_ENCODED.getMimeType()}
+        );
+        HttpHeaders httpHeaders = new HttpHeaders(headers);
+
+        HttpRequest request = new HttpRequest(
+                requestLine,
+                httpHeaders,
+                new HttpRequestBody(urlEncodedString.getBytes(StandardCharsets.UTF_8))
+        );
+
+        // when
+        HttpResponse response = new HttpResponse();
+        LoginHandler.getInstance().handle(request, response);
+
+        // then
+
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+        assertThat(response.getBody()).containsExactly(FileReader.readFile(expectedPath));
+
+        Map<String, String> responseHeaders = response.getHeaders();
+        assertEquals(
+                responseHeaders.get(HttpHeader.CONTENT_TYPE.getValue()),
+                ContentTypes.TEXT_HTML.getMimeType()
+        );
+        String sessionId = responseHeaders.get(HttpHeader.SET_COOKIE.getValue());
+        assertNotNull(sessionId);
+    }
+}
